2bfdd37ca962c7c005d73a4b198131b56e99acd4
Dongni Wang <dwang022@gmail.com>
2016-04-01 15:09:57 -0400

    add leaderAddr

--- dchat/DNode.h
+++ dchat/DNode.h
@@ -74,7 +74,7 @@ public:
         std::string myAddr = dNet->DNinit();
         memberNode = new Member(myAddr);        // Create Member node
         joinAddress = new Address(addr);        // JoimyAddressn address
-        std::cout << username << " join an existing chat, listening on "
+        std::cout << username << " started a new chat, listening on "
             << memberNode->getAddress() << std::endl;
     }
 
